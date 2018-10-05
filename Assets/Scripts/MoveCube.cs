04db8614ab3fcf7c3f807f6b56457d974a296dc5
Jack Schlesinger <jackschlesinger@gmail.com>
2018-10-05 11:15:46 -0400

    Added movement controls

--- Assets/Scripts/MoveCube.cs
+++ Assets/Scripts/MoveCube.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class MoveCube : MonoBehaviour
 {
 
-	public Vector3 cubeSpeed = Vector3.up;
+	public float cubeSpeed = 1.0f;
 	
 	// Use this for initialization
 	void Start () {
@@ -13,7 +13,24 @@ public class MoveCube : MonoBehaviour
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+	{
+		var position = transform.position;
+		if (Input.GetKeyDown(KeyCode.UpArrow))
+		{
+			transform.position = position * cubeSpeed * Vector3.up;
+		}	
+		else if (Input.GetKeyDown(KeyCode.DownArrow))
+		{
+			transform.position = position * cubeSpeed * Vector3.down;
+		}	
+		else if (Input.GetKeyDown(KeyCode.LeftArrow))
+		{
+			transform.position = position * cubeSpeed * Vector3.left;
+		}	
+		else if (Input.GetKeyDown(KeyCode.RightArrow))
+		{
+			transform.position = position * cubeSpeed * Vector3.right;
+		}	
 	}
 }
