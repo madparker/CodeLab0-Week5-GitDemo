49295e43dcb811f1ff28397a8f7818eb194abea9
Matthew Parker <maddparker@gmail.com>
2018-10-11 23:38:20 -0400

    Changed Timer Update

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -15,13 +15,5 @@ public class Timer : MonoBehaviour
 	
 	// Update is called once per frame
 	void Update () {
-		if (timeElapsed < duration)
-		{
-			timeElapsed += Time.deltaTime;
-		}
-		else
-		{
-			Debug.Log("Times up!");
-		}
 	}
 }
