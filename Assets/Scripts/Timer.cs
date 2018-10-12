fca94ae1e20118d79242048e2e32dbe691ab602e
Matthew Parker <maddparker@gmail.com>
2018-10-11 23:37:35 -0400

    Made Timer Better

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -2,8 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Timer : MonoBehaviour {
+public class Timer : MonoBehaviour
+{
 
+	public float duration = 5;
+	float timeElapsed = 0;
+	
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +15,13 @@ public class Timer : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (timeElapsed < duration)
+		{
+			timeElapsed += Time.deltaTime;
+		}
+		else
+		{
+			Debug.Log("Times up!");
+		}
 	}
 }
