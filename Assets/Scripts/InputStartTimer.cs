add6c6a3b93f8a5b42c346560e3d9197da2dee7e
Matthew Parker <maddparker@gmail.com>
2018-10-12 11:33:29 -0400

    Buffed my Timer

---
+++ Assets/Scripts/InputStartTimer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputStartTimer : MonoBehaviour
+{
+
+	public Timer timer;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		timer = GameObject.Find("Cube").GetComponent<Timer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyUp(KeyCode.Space))
+		{
+			timer.Trigger();
+		}
+	}
+}
--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,28 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class Timer : MonoBehaviour
 {
 
-	public float duration = 5;
+	public float duration = 7;
+	public bool activated = false;
+	
 	float timeElapsed = 0;
+	
 	
 	// Use this for initialization
 	void Start () {
 		
 	}
-	
+
+	public void Trigger()
+	{
+		activated = true;
+		timeElapsed = 0;
+	}
+
+	void WhileTimerRunning()
+	{
+		Debug.Log("Timer is running: " + timeElapsed);
+	}
+
 	// Update is called once per frame
 	void Update () {
-		if (timeElapsed < duration)
+		if (activated)
 		{
-			timeElapsed += Time.deltaTime;
-			Debug.Log("Timer is running!");
+			if (timeElapsed < duration)
+			{
+				timeElapsed += Time.deltaTime;
+				WhileTimerRunning();
+			}
+			else
+			{
+				StopTimer();
+			}
 		}
-		else
-		{
-			Debug.Log("Times up!");
-		}
+	}
+
+	void StopTimer()
+	{
+		Debug.Log("Times up!");
+		activated = false;
+		timeElapsed = 0;
 	}
 }
